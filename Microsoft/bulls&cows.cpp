d6d078141396e624b002f269e53c02af3fd09c04
swastik2703 <swastikvasistha2127@gmail.com>
2023-01-02 20:30:02 +0530

    Bulls & cows

---
+++ Microsoft/bulls&cows.cpp
@@ -0,0 +1,55 @@
+// You are playing the Bulls and Cows game with your friend.
+
+// You write down a secret number and ask your friend to guess what the number is. When your friend makes a guess, you provide a hint with the following info:
+
+// The number of "bulls", which are digits in the guess that are in the correct position.
+// The number of "cows", which are digits in the guess that are in your secret number but are located in the wrong position. Specifically, the non-bull digits in the guess that could be rearranged such that they become bulls.
+// Given the secret number secret and your friend's guess guess, return the hint for your friend's guess.
+
+// The hint should be formatted as "xAyB", where x is the number of bulls and y is the number of cows. Note that both secret and guess may contain duplicate digits.
+/*
+Input: secret = "1807", guess = "7810"
+Output: "1A3B"
+Explanation: Bulls are connected with a '|' and cows are underlined:
+"1807"
+  |
+"7810"*/
+
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls = 0,cows=0;
+        //dono vector bna lo
+        vector<int> s(10,0);
+        vector<int> g(10,0);
+
+        //secret ko traverse kro
+        for(int i=0; i<secret.length(); i++){
+            if(secret[i] == guess[i]){
+                bulls++;
+            }
+            else{
+                //vector ko update krdo agar match nhi krti index
+                s[secret[i] - '0']++;
+                g[guess[i] - '0']++;
+            }
+        }
+
+        //cows count kro dono vector ke minimum nikalo or sum krdo
+        for(int i=0; i<10; i++){
+            cows += min(s[i],g[i]);
+        }
+
+        string ans="";
+        ans += to_string(bulls);
+        ans += ('A');
+        ans += to_string(cows);
+        ans += ('B');
+
+        return ans;
+    }
+};
